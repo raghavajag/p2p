5fe8e9bbf7ed2ba2a65fdeb01dccc509f0116892
raghavajag <raghavajagarwal@gmail.com>
2023-11-06 13:26:26 +0530

    feat: handle tcp conn

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"p2p/deck"
-)
+import "p2p/p2p"
 
 func main() {
-	deck := deck.New()
-	for i := 0; i < 52; i++ {
-		fmt.Println(deck[i])
-	}
+	// deck := deck.New()
+	// for i := 0; i < 52; i++ {
+	// 	fmt.Println(deck[i])
+	// }
+	server := p2p.NewServer(p2p.ServerConfig{
+		ListenAddr: ":8080",
+	})
+	server.Start()
 }
---
+++ p2p/server.go
@@ -0,0 +1,78 @@
+package p2p
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+type Peer struct {
+	conn       net.Conn
+	listenAddr string
+}
+type ServerConfig struct {
+	ListenAddr string
+}
+type Server struct {
+	peers    map[string]*Peer
+	listener net.Listener
+	ServerConfig
+	addPeer chan *Peer
+}
+
+func NewServer(cfg ServerConfig) *Server {
+	return &Server{
+		peers:        make(map[string]*Peer),
+		ServerConfig: cfg,
+		addPeer:      make(chan *Peer),
+	}
+}
+
+func (s *Server) Start() {
+	go s.loop()
+	if err := s.listen(); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Listening on %s\n", s.ListenAddr)
+	s.acceptLoop()
+}
+func (s *Server) handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		receivedData := scanner.Text()
+		fmt.Printf("Received: %s\n", receivedData)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading from %s: %s\n", conn.RemoteAddr(), err)
+	}
+}
+func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go s.handleConn(conn)
+	}
+}
+func (s *Server) listen() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+	if err != nil {
+		return err
+	}
+	s.listener = ln
+	return nil
+}
+func (s *Server) loop() {
+	for {
+		select {
+		case peer := <-s.addPeer:
+			s.peers[peer.listenAddr] = peer
+			fmt.Printf("Peer %s connected\n", peer.listenAddr)
+		}
+	}
+}
